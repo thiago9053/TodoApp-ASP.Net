e0d920991971a29da5e6786e24211a5be4b99d84
thiago9053 <thiago.tl9053@gmail.com>
2023-11-12 22:43:57 +0700

    Add project files.

---
+++ TodoApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoTask> Tasks { get; set; }
+    }
+}
---
+++ TodoApp/Models/TodoTask.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class TodoTask
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedDate { get; set;}
+    }
+}
